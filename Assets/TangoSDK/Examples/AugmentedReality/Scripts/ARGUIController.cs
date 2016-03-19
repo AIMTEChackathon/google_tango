2b7cd8f23a243e9047d6f2099abfc30f94cbd72b
Jiri Prokop <jprokop@samepage.io>
2016-03-19 19:24:47 +0100

    Menu added

--- Assets/TangoSDK/Examples/AugmentedReality/Scripts/ARGUIController.cs
+++ Assets/TangoSDK/Examples/AugmentedReality/Scripts/ARGUIController.cs
@@ -101,9 +101,34 @@ public class ARGUIController : MonoBehaviour, ITangoLifecycle, ITangoDepth
     /// </summary>
     public void OnGUI()
     {
-	if (GUI.Button (new Rect (10,70, 100, 20), "This is text"))
+	float centerX = (Screen.width / 2);
+	float centerY = (Screen.height / 2);
+	float menuWidth = 100;
+	float menuHeight = 150;
+	float menuX = centerX - (menuWidth / 2);
+	float menuY = centerY - (menuHeight / 2);
+	float menuPadding = 10;
+	float buttonStartY = menuY + menuPadding * 3;
+	float buttonWidth = 80;
+	float buttonHeight = 20;
+	float buttonSpacing = 30;
+
+	GUI.Box(new Rect(menuX, menuY, menuWidth, menuHeight), "Choose:");
+	if (GUI.Button (new Rect (menuX + menuPadding, buttonStartY + buttonSpacing * 0, buttonWidth, buttonHeight), "Option 1"))
 	{
-		
+		print ("option 1");
+	}
+	if (GUI.Button (new Rect (menuX + menuPadding, buttonStartY + buttonSpacing * 1, buttonWidth, buttonHeight), "Option 2"))
+	{
+		print ("option 2");
+	}
+	if (GUI.Button (new Rect (menuX + menuPadding, buttonStartY + buttonSpacing * 2, buttonWidth, buttonHeight), "Option 3"))
+	{
+		print ("option 3");
+	}
+	if (GUI.Button (new Rect (menuX + menuPadding, buttonStartY + buttonSpacing * 3, buttonWidth, buttonHeight), "Option 4"))
+	{
+		print ("option 4");
 	}
 
         if (m_selectedMarker != null)
